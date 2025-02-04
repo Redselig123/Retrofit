1efdb7b7f010a5842552d859bb6ac64e573131e1
Redselig123 <facucancian@yahoo.com.ar>
2025-02-04 11:33:07 -0300

    added okhttp

--- app/src/main/java/com/example/retrofit/api/RetrofitCLient.kt
+++ app/src/main/java/com/example/retrofit/api/RetrofitCLient.kt
@@ -1,16 +1,26 @@
 package com.example.retrofit.api
 
 import com.example.retrofit.interfaces.ApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
 
+    // Configuramos OkHttp con el interceptor
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
     val instance: ApiService by lazy {
         val retrofit =
             Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
